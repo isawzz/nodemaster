2e7c94d73501bfa62b874fdb978e135d333f7dec
isawzz <iwgajam@gmail.com>
2022-09-11 11:15:05 -0700

    das

--- nature/baustelle.js
+++ nature/baustelle.js
@@ -27,16 +27,16 @@ function lsys_init(offx = 0, offy = 0, options = {}) {
 	let laus = RULES.map(x => x.aus).join();
 	let lwird = RULES.map(x => x.wird).join();
 	//console.log('laus', laus, 'lwird', lwird);
-	let naus=countAll(laus,'ABF');
-	let nwird=countAll(lwird,'ABF');
-	let ratio = nwird/naus;
+	let naus = countAll(laus, 'ABF');
+	let nwird = countAll(lwird, 'ABF');
+	let ratio = nwird / naus;
 	//console.log('naus',naus,'nwird',nwird,'ratio',ratio);
 
 	//2^7 ist ok
 
 	let maxnodes = 3000;
-	let pow=2;
-	while(Math.pow(ratio,pow)<maxnodes) pow++;
+	let pow = 2;
+	while (Math.pow(ratio, pow) < maxnodes) pow++;
 	//console.log('max depth is:',pow,'bei ratio',ratio)
 
 	let root = {
@@ -63,26 +63,33 @@ function lsys_init(offx = 0, offy = 0, options = {}) {
 		jitter: false,
 	};
 
-	console.log('depth is',root.depth)
+	console.log('depth is', root.depth)
 	return root;
 }
 function lsys_add() {
 	let root = C.root;
 	root.gen++;
-	root.len*=.6;
-	root.sentence = generate();
+
+	let len = 100;
+	let gens = 4;
+	for (let i = 0; i < gens; i++) {
+		root.len = len *= .5;
+		root.sentence = generate();
+	}
+
+	// root.len*=.10;	root.sentence = generate();	root.sentence = generate();
 	sentence2tree();
 	console.log('root', root.gen, root.sentence, C.items.branch);
 	//C.changed = true;
-	if (root.gen >= root.depth) console.log('DONE!'); else setTimeout(lsys_add, 100);
+	//if (root.gen >= root.depth) console.log('DONE!'); else setTimeout(lsys_add, 100);
 }
-
 function sentence2tree() {
 	C.items = {};
 	let root = C.root;
 	const damag = toRadian(25);
 	let [sentence, len, angle, dlen, da, gen] = [root.sentence, root.len, root.angle, 1, toRadian(25), 0];
 	let id = 0;
+	angle = toRadian(90);
 	let b = root, stack = []; b.id = id++; b.age = 0;
 	for (var i = 0; i < sentence.length; i++) {
 		var ch = sentence.charAt(i);
@@ -91,19 +98,18 @@ function sentence2tree() {
 			b = create_branchL(b, angle, len, rColor()); lookupAddToList(C.items, ['branch'], b);
 			b.id = id++;
 		} else if (ch == '+') {
-			angle+=damag; //da=damag; //angle = b.angle - da; //toRadian(25); //rotate(angle);
+			angle += toRadian(20); //damag; //da=damag; //angle = b.angle - da; //toRadian(25); //rotate(angle);
 		} else if (ch == '-') {
-			angle-=damag; //da=-damag; //angle = b.angle + da; //toRadian(25); //da/2; //rotate(-angle);
+			angle -= toRadian(17); //damag; //da=-damag; //angle = b.angle + da; //toRadian(25); //da/2; //rotate(-angle);
 		} else if (ch == '[') {
 			stack.push(b);
 			//console.log('stack',stack.map(x=>x.id))
 		} else if (ch == ']') {
-			b = stack.pop(); angle=b.angle;
+			b = stack.pop(); angle = b.angle;
 		}
 	}
 	C.changed = true;
 }
-
 function create_branchL(b, angle, len, color) { //p,len, angle,gen) {
 	//let len = b.len * root.dlen;
 	let root = C.root;
@@ -130,7 +136,6 @@ function create_branchL(b, angle, len, color) { //p,len, angle,gen) {
 	return o;
 
 }
-
 function generate() {
 	let root = C.root;
 	let rules = root.rules;
