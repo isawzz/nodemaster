b0406d996c0137022b4975a9b7f713f8b98deaef
isawzz <iwgajam@gmail.com>
2022-10-28 06:09:07 -0700

    das

--- caba/start.js
+++ caba/start.js
@@ -20,7 +20,18 @@ async function start() {
 		list.push(word);
 	}
 
-	console.log('list',list)
+	console.log('list',list);
+
+	dTable = mBy('map');
+	let animations=['beat','bounce','fade','flip','pulse','shake','spin'];//,'beat-fade','spin-pulse']
+	let iani=0;
+	for (const w of arrTake(list,25)){
+
+		let an = 'pulse';// animations[iani++]; if (iani>animations.length) iani=0; // rChoose(animations);
+		let d=mDiv(dTable,{align:'center',bg:'blue',margin:8,fg:'white',display:'inline-block',padding:10},null,`<i class="fa fa-${w} fa-${an} fa-2x"></i><br>${w}`);
+		
+	}
+
 	//test12_europe(); //faicon_list(); //test13_get_the_div(); //test12_europe();//gTest13(); //test6_click(); //test5_showroute();
 	//gTest05(); //test11_gengraph();//test10_autocomplete(); //test9_google(); //test8_load_googlemap_in_iframe(); //test7(); //test6_click(); //test5_showroute(); //test4_tools(); //test3_better_agent(); // test2_two_maps | test0_canvas_overlay
 }
