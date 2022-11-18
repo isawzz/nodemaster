b08e5ff604574134a65917df24c1d9b017ec6eca
isawzz <iwgajam@gmail.com>
2022-11-18 12:45:42 -0800

    das

--- cai/baustelle.js
+++ cai/baustelle.js
@@ -1,53 +1,50 @@
 
 DA.edits = [];
-function add_edit(x,y,text='',bg='random'){
-	let d = mDiv(dTable, { bg:bg, fg:'contrast', x: x, y: y, padding:10, position: 'absolute', wmin:10,  }, getUID(), text); //,'<span contenteditable="true">This is an editable paragraph.</span>');
+function add_edit(x, y, text = '', bg = 'random') {
+	let d = mDiv(dTable, { bg: bg, fg: 'contrast', x: x, y: y, position: 'absolute', padding: 10, wmin: 10, }, getUID(), text); //,'<span contenteditable="true">This is an editable paragraph.</span>');
 	DA.edits.push(d);
 	d.setAttribute('contentEditable', true);
 	d.style.outline = 'none';
-	d.onkeydown=function (e) {
+	d.onkeydown = function (e) {
 		DA.tabKeyPressed = e.keyCode == 9;
 		if (DA.tabKeyPressed) {
 			e.preventDefault();
 			return;
-		}else {
+		} else {
 			//console.log('keyCode',e.keyCode);
 			//if (e.keyCode == 13){			save_all();			}
 		}
 	};
-	d.onkeyup=function (e) {
+	d.onkeyup = function (e) {
 		if (DA.tabKeyPressed) {
 			//console.log('TAB!', DA.edits); // Do stuff for TAB
 			//what is the index of this div?
-			let idx=DA.edits.indexOf(e.target);
+			let idx = DA.edits.indexOf(e.target);
 			//console.log('i am index',idx);
-			let next = (idx+1)%DA.edits.length;
-			if (next!=idx) DA.edits[next].focus();
+			let next = (idx + 1) % DA.edits.length;
+			if (next != idx) DA.edits[next].focus();
 			e.preventDefault();
 			return;
 		}
-		
+
 
 		//Do other stuff when not TAB
 	};
-	d.onfocus=e=>{
-		
+	d.onfocus = e => {
+
 		//console.log('target',e.target.id);
 		//return;
-		if (DA.focusElement != e.target && isdef(DA.focusElement)){
+		if (DA.focusElement != e.target && isdef(DA.focusElement)) {
 			let el = DA.focusElement;
 			if (isEmpty(el.innerHTML)) {
-				removeInPlace(DA.edits,el);
+				removeInPlace(DA.edits, el);
 				el.remove();
 				//console.log('recycled:',el);
 			}
 		}
 
 		//console.log('target',e.target);
-		let id = evToId(e);
-		console.log('id',id)
-		let elem = mBy(id);
-		selectText(elem);
+		let id = evToId(e); console.log('id', id); let elem = mBy(id); selectText(elem);
 
 		//console.log('focus');
 		DA.focusElement = e.target;
@@ -57,39 +54,39 @@ function add_edit(x,y,text='',bg='random'){
 	};
 	d.focus();
 }
-async function load_all(){
-	let o= await route_path_yaml_dict('../y/page.yaml');
-	for(const item of o){		add_edit(item.x,item.y-41,item.text);	}
+async function load_all() {
+	let o = await route_path_yaml_dict('../y/page.yaml');
+	for (const item of o) { add_edit(item.x, item.y - 41, item.text); }
 }
-function onclick_toobar(name) { 
-	console.log('clicked', name); 
-	switch(name){
-		case 'clear':mClear(dTable);DA.edits=[]; break;
-		case 'magic':break;
+function onclick_toobar(name) {
+	console.log('clicked', name);
+	switch (name) {
+		case 'clear': mClear(dTable); DA.edits = []; break;
+		case 'magic': break;
 		case 'lineup':
 			mCenterFlex(dTable); //mStyle(dTable,{display:'grid'});
-			DA.edits.map(x=>mStyle(x,{position:null,display:'inline'}));
-		break;
-		case 'orig':DA.edits.map(x=>x.style.position='absolute');break;
+			DA.edits.map(x => mStyle(x, { position: null, display: 'inline' }));
+			break;
+		case 'orig': DA.edits.map(x => x.style.position = 'absolute'); break;
 	}
 }
 function open_invisible_input(ev) {
 	if (ev.target.id != 'dTable') return;
 	let [x, y] = [ev.offsetX, ev.offsetY];
-	y=toModulo(y-10,20);
-	x=toModulo(x,50);
-	add_edit(x,y);
+	y = toModulo(y - 10, 20);
+	x = toModulo(x, 50);
+	add_edit(x, y);
 }
-function save_all(){
+function save_all() {
 	//console.log('save',Date.now())
 	let data = [];
-	for(const edit of DA.edits){
+	for (const edit of DA.edits) {
 		let rect = getRect(edit);
 		let text = edit.innerHTML;
-		let o = {x:rect.x,y:rect.y,text:text};
+		let o = { x: rect.x, y: rect.y, text: text };
 		data.push(o);
 	}
-	route_post_json('http://localhost:3000/post/json', {data:data,filename:'page'}, response => {
+	route_post_json('http://localhost:3000/post/json', { data: data, filename: 'page' }, response => {
 		//console.log(JSON.stringify(response));
 	});
 }
--- cai/magic.js
+++ cai/magic.js
@@ -1,21 +1,26 @@
 
-function get_bstp(){
+function get_bstp() {
 	const actions = {
-		obst:'apfel orange assorted plum granat',
-		veggie:'karotte kartoffel purree kohlsprossen zwiebel knoblauch assorted',
-		backen:'almondhorn gugelhupf vanillekipferl striezelteig apfelmus marm',
-		kochen:'purree hameggs omelette soup mushroom pizza',
-		spaz:'femto micro macro standard 104 tennis HS safeway libTJ',
+		obst: 'apfel orange assorted plum granat',
+		veggie: 'karotte kartoffel purree kohlsprossen zwiebel knoblauch assorted',
+		backen: 'almondhorn gugelhupf vanillekipferl striezelteig apfelmus marm',
+		kochen: 'purree hameggs omelette soup mushroom pizza',
+		spaz: 'femto micro macro standard 104 tennis HS safeway libTJ',
 		klavier: 'op111 wald1 mond3 etude nocturne ballade ital mozartB mozartKKC kk3_3 kk5_2',
 		putzen: 'herd counter kueche eingang guklo stiege loft/guzi wohnzi',
-		erltm: 'post zahlen scan emailtm discordtm',
+		erltm: 'post zahlen scan emailtm discordtm termin',
+		deconstruction: 'video sleep walkthink libthink starbucksthink',
 	};
-	let time=rNumber(1,5);
-	let points=rNumber(1,3);
+	let time = rNumber(1, 5);
+	let points = rNumber(1, 3);
 	//you have to fall in love with stillness
 	//stillness is: handeln ohne absicht - avoiding harm means avoiding ego
 
-	let key=rChoose(get_keys(actions));
+	let key = rChoose(get_keys(actions));
+	let val = rChoose(actions[key]);
+
+	console.log('val', val);
+	add_edit()
 
 }
 
