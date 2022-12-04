77eb42074e304fc2e5e42893b0e4ae3f571c15ab
isawzz <iwgajam@gmail.com>
2022-12-04 09:04:58 -0800

    das

---
+++ all/testmongo/start.js
@@ -0,0 +1,27 @@
+onload = start;
+
+function start() {
+	dTable = document.body;
+	mStyle(dTable, {
+		//bg:'red',
+		h:'100vh',
+		margin:0,
+		padding:0,
+		'background-image': "url('../base/assets/img/airport/airport2.jpg')", //'url(airport1.jpg)', //'../base/assets/img/airport/airport1.jpg',
+		'background-position': 'center bottom', 
+		'background-repeat': 'no-repeat',
+		'background-size': 'stretch'
+	});
+	//dTable.style.backgroundImage = './'
+}
+
+
+
+
+
+
+
+
+
+
+
